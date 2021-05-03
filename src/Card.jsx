941ec39e77724b1e72184f89084fc9570d5d34ce
RahulGautamSingh <rahul_ug@cse.nits.ac.in>
2021-05-03 12:55:16 +0545

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,28 +1,11 @@
 function Card(props) {
   return (
-   <>
-      <div
-        className="card" id="card1"
-        style={{ backgroundColor: props.color }}
-      ></div>
-      <div
-        className="card "id="card2"
-        style={{ backgroundColor: props.color }}
-      ></div>
-      <div
-        className="card "id="card3"
-        style={{ backgroundColor: props.color }}
-      ></div>
-      <div
-        className="card "id="card4"
-        style={{ backgroundColor: props.color }}
-      ></div>
-      <div
-        className="card"id="card5"
-        style={{ backgroundColor: props.color }}
-      ></div>
- </>
+    <div
+      className="card"
+      style={{ backgroundColor: props.color, opacity: props.opacity }}
+    ></div>
   );
 }
 
 export default Card;
+
